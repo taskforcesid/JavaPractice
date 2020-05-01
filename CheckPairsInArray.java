88364664174fb8c9eb69c11ffd523f2d0b03f496
taskforcesid <52729233+taskforcesid@users.noreply.github.com>
2020-05-01 04:31:33 -0700

    Update CheckPairsInArray.java

--- CheckPairsInArray.java
+++ CheckPairsInArray.java
@@ -4,28 +4,38 @@ import java.util.*;
 public class CheckPairsInArray
 {
     
-    //How to find all pairs in an array of integers whose sum is equal to the given number
+    //Complete the pairs function below. It must return an integer representing 
+	//the number of element pairs having the required difference.
+
+pairs has the following parameter(s):
+
+k: an integer, the target difference
+arr: an array of integers
     
     public static HashMap CheckPairs  (int [] A, int sum){
         
+    static int pairs( int[] A, int sum) {
         HashMap<Integer,Integer> myMap = new HashMap<Integer,Integer>();
         HashMap results = new HashMap();
+        int count = 0;
         for(int i=0; i < A.length; i++)
         {
-            int value = sum - A[i];
-            myMap.put(A[i], value);
-            
-            if(myMap.containsKey(value))
-            {
-                int cache = myMap.get(value);
-                if(cache == A[i])
+            myMap.put(A[i], A[i]-sum);
+        }
+        for(int i=0; i < A.length; i++)
+        {
+            if(myMap.containsKey(A[i]))
+            {   int value = A[i]-sum;
+                if (myMap.containsKey(value))
                 {
-                    results.put(value, A[i]);
+                    count++;
                 }
             }
         }
-        return results;
+
+        return count;
     }
+
 
 	public static void main(String[] args) {
 	    int [] A = {11,2,3,4,5,6,12, 9, 14};
